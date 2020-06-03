ac70213d1b930288fca34a7821ba9eedc72b25b4
Kumar, Shubham <shubham.snu@gmail.com>
2020-06-03 23:40:24 +0530

    Added Authentication

---
+++ taskmanager/src/main/java/com/slackhack/taskmanager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.slackhack.taskmanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.slackhack.taskmanager.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByUserName(String username);
+
+}
